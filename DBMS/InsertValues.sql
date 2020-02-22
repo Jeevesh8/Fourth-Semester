e98d6274104d5d36f034d8882d8c60070bac67ee
Jeevesh8 <creativityinczenyoga@gmail.com>
2020-02-23 00:46:12 +0530

    Schedule for deleting old rows added

--- DBMS/InsertValues.sql
+++ DBMS/InsertValues.sql
@@ -8,6 +8,8 @@ INSERT ALL
     VALUES('KOLKATTA', 'MADRAS', 2, '08-MAR-2020', '10-MAR-2020')
     INTO Trip
     VALUES('AMRITSAR', 'SHANGHAI', 3, '08-MAR-2020', '12-MAR-2020')
+    INTO Trip
+    VALUES('DELHI', 'MADURAI', 4, '01-JAN-2019', '05-JAN-2019')
     
     INTO Customer
     VALUES(0,9205677801,'B-32, Green Park', 'sjsjs@gmail.com', 'Aman', 'Chauhan',2)
@@ -17,6 +19,8 @@ INSERT ALL
     VALUES(2,9105637801,'A-34, Green Park', 'vermajs@gmail.com', 'Amit', 'Verma',0)
     INTO Customer
     VALUES(3,9103637801,'A-33, Green Park', 'sharmajs@gmail.com', 'Amit', 'Sharma',1)
+    INTO Customer
+    VALUES(4,8203637801,'A-13, Green Park', 'sharma11js@gmail.com', 'Akshit', 'Sharma',4)
     
     INTO Driver
     VALUES(0,0.0,'A-35, Green Park', 9311061144, 'Suresh', 'Kumar', 2)
@@ -24,7 +28,7 @@ INSERT ALL
     VALUES(1,0.0,'AF-33, Green Park', 9311061244, 'Yogesh', 'Kumar', 0)
     INTO Driver
     VALUES(2,0.0,'AS-33, Green Park', 9313061144, 'Narendra', 'Kumar', 3)
-    
+
     INTO Vehicle
     VALUES('K-2',4,123734534,0,1)
     INTO Vehicle
@@ -35,10 +39,11 @@ INSERT ALL
     VALUES('M-2',5,123774534,3,2)
 
     INTO Payment
-    VALUES(6000,63742,'Debit Card',1,3)
+    VALUES(6000, 63742, 'Debit Card', 1, 3)
     INTO Payment
-    VALUES(8700,63827, 'Credit Card',2,0)
+    VALUES(8700, 63827, 'Credit Card', 2, 0)
     INTO Payment
-    VALUES(9200, 63372, 'Cash',0,2)
-
+    VALUES(9200, 63372, 'Cash', 0, 2)
+    INTO Payment
+    VALUES(9250, 23483, 'Cash', 4, 4)
 SELECT * FROM dual ;
---
+++ DBMS/delete_old_rows_job.sql
@@ -0,0 +1,22 @@
+-- Schedule for deleting rows older than 1 year automatically every day.
+
+CREATE OR REPLACE PROCEDURE delete_trips
+AS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Here');
+        DELETE FROM trip y where sysdate > add_months(y.arrival_date,12);
+    END;
+/
+BEGIN
+    DBMS_SCHEDULER.CREATE_JOB(
+        job_name        => 'delete_old_rows',
+        job_type        => 'PLSQL_BLOCK',
+        job_action      => 'delete_trips;',
+        start_date      => sysdate,
+        repeat_interval => 'FREQ=DAILY',
+        end_date        => add_months(sysdate,12*10),
+        auto_drop       => FALSE,
+        enabled         => TRUE
+    );
+END;
+/
